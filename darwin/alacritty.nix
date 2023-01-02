4d3ae6dde1d1b8cbd90cb17ea2a43579496bff1b
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-01-02 21:13:07 +0200

    gruvbox colors

--- darwin/alacritty.nix
+++ darwin/alacritty.nix
@@ -39,33 +39,43 @@
 
       colors = {
         primary = {
-          background = "0x1a1b26";
-          foreground = "0xc0caf5";
+          background = "#282828";
+          foreground = "#fbf1c7";
+          bright_foreground = "#f9f5d7";
+          dim_foreground = "#f2e5bc";
+        };
+        cursor = {
+          text = "CellBackground";
+          cursor = "CellForeground";
+        };
+        vi_mode_cursor = {
+          text = "CellBackground";
+          cursor = "CellForeground";
+        };
+        selection = {
+          text = "CellBackground";
+          background = "CellForeground";
         };
         normal = {
-          black = "0x15161e";
-          red = "0xf7768e";
-          green = "0x9ece6a";
-          yellow = "0xe0af68";
-          blue = "0x7aa2f7";
-          magenta = "0xbb9af7";
-          cyan = "0x7dcfff";
-          white = "0xa9b1d6";
+          black = "#282828";
+          red = "#cc241d";
+          green = "#98971a";
+          yellow = "#d79921";
+          blue = "#458588";
+          magenta = "#b16286";
+          cyan = "#689d6a";
+          white = "#a89984";
         };
         bright = {
-          black = "0x414868";
-          red = "0xf7768e";
-          green = "0x9ece6a";
-          yellow = "0xe0af68";
-          blue = "0x7aa2f7";
-          magenta = "0xbb9af7";
-          cyan = "0x7dcfff";
-          white = "0xc0caf5";
+          black = "#928374";
+          red = "#fb4934";
+          green = "#b8bb26";
+          yellow = "#fabd2f";
+          blue = "#83a598";
+          magenta = "#d3869b";
+          cyan = "#8ec07c";
+          white = "#ebdbb2";
         };
-        indexed_colors = [
-          { index = 16; color = "0xff9e64"; }
-          { index = 17; color = "0xdb4b4b"; }
-        ];
       };
     };
   };
