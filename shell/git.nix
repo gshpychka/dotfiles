5229a163d72db5f4e284f65626873d86b21d2433
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-01-02 19:17:00 +0200

    create SSH agent config

--- shell/git.nix
+++ shell/git.nix
@@ -1,10 +1,17 @@
 { config, pkgs, lib, ... }: {
-  #file = {
-  #  gitSigningKey = {
-  #    source = fetchurl https://github.com/gshpychka.keys;
-  #    target = ".ssh/id_ed25519.pub";
-  #  };
-  #};
+  home.file = {
+    #gitSigningKey = {
+    #  source = fetchurl https://github.com/gshpychka.keys;
+    #  target = ".ssh/id_ed25519.pub";
+    #};
+    ".ssh/config" = {
+      target = ".ssh/config";
+      text = ''
+      Host *
+	IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      '';
+    };
+  };
   programs.git = {
     enable = true;
     delta.enable = true;
