4e2b56fbda93a18f035713f39af8af7f8e9734d8
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-06-29 17:01:38 +0300

    add default mapping for incremental search

--- home-manager/eve/neovim/config/after/plugin/treesitter.lua
+++ home-manager/eve/neovim/config/after/plugin/treesitter.lua
@@ -10,6 +10,7 @@ require("nvim-treesitter.configs").setup({
 	incremental_selection = {
 		enable = true,
 		keymaps = {
+			init_selection = "gnn", -- default mapping
 			node_incremental = "v",
 			node_decremental = "V",
 		},
