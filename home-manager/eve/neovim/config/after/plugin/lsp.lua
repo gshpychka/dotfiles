1e84dfe56c096800227e483ada80efc14e542184
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-07-10 13:03:33 +0300

    disable eslint diagnostics in null-ls

--- home-manager/eve/neovim/config/after/plugin/lsp.lua
+++ home-manager/eve/neovim/config/after/plugin/lsp.lua
@@ -253,7 +253,7 @@ null_ls.setup({
 		null_ls.builtins.code_actions.eslint_d,
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.code_actions.statix,
-		null_ls.builtins.diagnostics.eslint_d,
+		-- null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.diagnostics.flake8,
 		null_ls.builtins.diagnostics.jsonlint,
 		-- null_ls.builtins.diagnostics.mypy,
