058aa105918a4eaa5030049d54fc8f4b39b42546
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-07-11 22:14:10 +0300

    switch to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   # ./result/sw/bin/darwin-rebuild switch --flake ".#eve"
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
