37e8cdf66b7f9967751a48b14ca40db3481cc3aa
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-07-30 14:22:09 +0300

    change keymap

--- home-manager/eve/neovim/config/after/plugin/harpoon.lua
+++ home-manager/eve/neovim/config/after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "open harpoon menu" })
 
 -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
