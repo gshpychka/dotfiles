51e53e42dbd1126a44494130df2beebcfa35662b
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-08-21 10:51:00 +0300

    nvim: cmp CR mapping

--- home-manager/eve/neovim/config/after/plugin/lsp.lua
+++ home-manager/eve/neovim/config/after/plugin/lsp.lua
@@ -119,6 +119,17 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-l>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping({
+			i = function(fallback)
+				if cmp.visible() and cmp.get_active_entry() then
+					cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+				else
+					fallback()
+				end
+			end,
+			s = cmp.mapping.confirm({ select = true }),
+			c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+		}),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
