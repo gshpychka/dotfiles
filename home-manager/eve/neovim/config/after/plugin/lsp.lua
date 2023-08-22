efbf4ba4fccff1172f5d1a70294ad06b695efe93
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-08-22 16:41:27 +0300

    nvim: remove gitsigns code actions

--- home-manager/eve/neovim/config/after/plugin/lsp.lua
+++ home-manager/eve/neovim/config/after/plugin/lsp.lua
@@ -289,7 +289,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.code_actions.eslint_d,
-		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.code_actions.statix,
 		-- null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.diagnostics.flake8,
