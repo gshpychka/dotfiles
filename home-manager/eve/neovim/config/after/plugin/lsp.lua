6bf21fcd1736113816692d30d87ce66dbc3de40e
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-08-28 13:27:14 +0300

    nvim: enable prettier formatting

--- home-manager/eve/neovim/config/after/plugin/lsp.lua
+++ home-manager/eve/neovim/config/after/plugin/lsp.lua
@@ -220,6 +220,7 @@ null_ls.setup({
 		null_ls.builtins.diagnostics.flake8,
 		null_ls.builtins.diagnostics.jsonlint,
 		-- null_ls.builtins.diagnostics.mypy,
+		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.eslint_d,
 		null_ls.builtins.formatting.fixjson,
 		null_ls.builtins.formatting.black,
