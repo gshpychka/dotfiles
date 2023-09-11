5898bc46fc494f30a3db59f15c6a4facab201ff5
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-09-11 12:08:23 +0300

    fix target and opener matching

--- home-manager/eve/finicky/config.js
+++ home-manager/eve/finicky/config.js
@@ -1,12 +1,33 @@
 module.exports = {
-  defaultBrowser: "Firefox",
+  defaultBrowser: ({ urlString }) => ({
+    name: "Firefox",
+    args: ["-P", "personal", urlString],
+  }), 
   options: {
     checkForUpdate: false
   },
   handlers: [
     {
-      match: ({ opener }) =>
-        ["Slack"].includes(opener.name),
+      match: ({ opener, url }) => {
+        const basedOnOpener =  [
+          "Slack",
+          "Leapp"
+        ].some(
+          appName => opener.path.toLowerCase().includes(
+            appName.toLowerCase()
+          )
+        );
+        const basedOnUrl = [
+          "aws.amazon.com",
+          "github.com",
+        ].some(
+          targetHostName => url.host.toLowerCase().includes(
+            targetHostName.toLowerCase()
+          )
+        );
+        finicky.log('Opening in work profile');
+        return basedOnOpener || basedOnUrl;
+      },
       browser: ({ urlString }) => ({
         name: "Firefox",
         args: ["-P", "work", urlString],
