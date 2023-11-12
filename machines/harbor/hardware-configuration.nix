51ba1e1e59cf40ef888744dd5317af07d462b2af
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2023-11-12 17:01:34 +0200

    harbor: remove SSD

--- machines/harbor/hardware-configuration.nix
+++ machines/harbor/hardware-configuration.nix
@@ -16,18 +16,11 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS";
+    device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
   };
 
-  fileSystems."/mnt/data" = {
-    device = "/dev/disk/by-label/DATA";
-    fsType = "ext4";
-  };
-
-  swapDevices = [
-    {device = "/dev/disk/by-label/SWAP";}
-  ];
+  swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
