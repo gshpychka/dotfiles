1bf107a7464fca391eda50918a154abd12f6a9c9
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2024-03-17 22:05:04 +0200

    feat: more telescope maps

--- home-manager/eve/neovim/config/after/plugin/telescope.lua
+++ home-manager/eve/neovim/config/after/plugin/telescope.lua
@@ -19,21 +19,9 @@ vim.keymap.set(
 )
 vim.keymap.set(
 	"n",
-	"<leader>fc",
-	builtin.commands,
-	{ desc = "Telescope commands" }
-)
-vim.keymap.set(
-	"n",
 	"<leader>fr",
 	builtin.lsp_references,
 	{ desc = "Telescope LSP references" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fhl",
-	builtin.highlights,
-	{ desc = "Telescope highlights" }
 )
 vim.keymap.set(
 	"n",
@@ -46,6 +34,58 @@ vim.keymap.set(
 	"<leader>fts",
 	builtin.treesitter,
 	{ desc = "Telescope Treesitter" }
+)
+
+-- Neovim helpers
+vim.keymap.set(
+	"n",
+	"<leader>fnc",
+	builtin.commands,
+	{ desc = "Telescope commands" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>fhl",
+	builtin.highlights,
+	{ desc = "Telescope highlights" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>fnk",
+	builtin.keymaps,
+	{ desc = "Telescope keymaps" }
+)
+
+-- Git
+vim.keymap.set(
+	"n",
+	"<leader>fgcc",
+	builtin.git_commits,
+	{ desc = "Telescope Git commits" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>fgcb",
+	builtin.git_bcommits,
+	{ desc = "Telescope Git buffer commits" }
+)
+vim.keymap.set(
+	"v",
+	"<leader>fgcb",
+	builtin.git_bcommits_range,
+	{ desc = "Telescope Git commits for range" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>fgb",
+	builtin.git_branches,
+	{ desc = "Telescope Git branches" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>fgs",
+	builtin.git_status,
+	{ desc = "Telescope Git status" }
 )
 
 require("telescope").setup({
