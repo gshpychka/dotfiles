a8b445f77ed2adcbc7e2d3b0c82250ee2bf58e44
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2024-03-23 16:19:38 +0200

    fix: disable auto-optimise-store

--- flake.nix
+++ flake.nix
@@ -92,7 +92,10 @@
               allowed-users = [user];
               trusted-users = ["root" user];
               experimental-features = ["nix-command" "flakes"];
-              auto-optimise-store = true;
+
+              # https://github.com/NixOS/nix/issues/7273
+              auto-optimise-store = false;
+
               # needed for devenv to enable cachix
               accept-flake-config = true;
             };
