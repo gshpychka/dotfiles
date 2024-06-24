115d82b1a7d70ee642c12c350f7cd7e08a46915a
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2024-06-24 16:52:26 +0300

    feat(reaper): nvidia drivers

--- machines/reaper/configuration.nix
+++ machines/reaper/configuration.nix
@@ -26,6 +26,31 @@
     gpgSmartcards.enable = true;
   };
 
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = true;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = false;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   users = {
     users = {
       gshpychka = {
@@ -45,10 +70,6 @@
     enable = true;
     enableCompletion = false;
     enableBashCompletion = false;
-  };
-
-  fonts = {
-    packages = with pkgs; [(nerdfonts.override {fonts = ["JetBrainsMono"];})];
   };
 
   security = {
