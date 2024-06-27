79adebc1f80e3b294b565ba4ae9ed4e5bfde1ea2
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2024-06-27 14:33:50 +0300

    chore: move code around, async formatting

--- home-manager/common/neovim/config/after/plugin/lsp.lua
+++ home-manager/common/neovim/config/after/plugin/lsp.lua
@@ -1,14 +1,10 @@
-function CreateOpts(bufnr, description)
-	return {
-		buffer = bufnr,
-		remap = false,
-		desc = description,
-	}
-end
-
 local on_attach = function(client, bufnr)
-	local function createOpts(description)
-		return CreateOpts(bufnr, description)
+	local function createOpts(bufnr, description)
+		return {
+			buffer = bufnr,
+			remap = false,
+			desc = description,
+		}
 	end
 
 	-- Mappings.
@@ -16,37 +12,49 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<leader>e", function()
 		vim.diagnostic.open_float()
 	end, createOpts("Open LSP diagnostics float"))
+
 	vim.keymap.set("n", "[d", function()
 		vim.diagnostic.goto_prev({ _highest = true })
 	end, createOpts("Go to previous LSP diagnostic"))
+
 	vim.keymap.set("n", "]d", function()
 		vim.diagnostic.goto_next({ _highest = true })
 	end, createOpts("Go to next LSP diagnostic"))
+
 	vim.keymap.set("n", "gd", function()
 		vim.lsp.buf.definition()
 	end, createOpts("Go to definition"))
+
 	vim.keymap.set("n", "gD", function()
 		vim.lsp.buf.type_definition()
 	end, createOpts("Go to the type definition"))
+
 	vim.keymap.set("n", "gi", function()
 		vim.lsp.buf.implementation()
 	end, createOpts("Go to implementation"))
+
 	vim.keymap.set("n", "gr", function()
 		vim.lsp.buf.references()
 	end, createOpts("Go to references"))
+
 	vim.keymap.set("n", "K", function()
 		vim.lsp.buf.hover()
 	end, createOpts("LSP hover"))
+
 	vim.keymap.set({ "n", "i" }, "<C-s>", function()
 		vim.lsp.buf.signature_help()
 	end, createOpts("Signature help"))
+
 	vim.keymap.set("n", "<leader>rn", ":IncRename ", createOpts("LSP rename"))
+
 	vim.keymap.set("n", "<leader>cda", function()
 		vim.lsp.buf.code_action()
 	end, createOpts("LSP code action"))
+
 	vim.keymap.set("n", "<leader>fo", function()
 		vim.lsp.buf.format({ timeout_ms = 5000 })
 	end, createOpts("LSP format"))
+
 	vim.keymap.set("n", "<leader>il", function()
 		vim.lsp.inlay_hint.enable(
 			not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
@@ -75,18 +83,11 @@ local on_attach = function(client, bufnr)
 		buffer = bufnr,
 		callback = function()
 			if client.server_capabilities.documentFormattingProvider then
-				vim.lsp.buf.format({ timeout_ms = 5000 })
+				vim.lsp.buf.format({ timeout_ms = 5000, async = true })
 			end
 		end,
 	})
-	-- if client.server_capabilities.signatureHelpProvider then
-	--     vim.api.nvim_create_autocmd({ "CursorHoldI" }, {
-	--         buffer=bufnr,
-	--         callback = function()
-	--             vim.lsp.buf.signature_help()
-	--         end,
-	--     })
-	-- end
+
 	vim.api.nvim_create_autocmd({ "CursorHold" }, {
 		buffer = bufnr,
 		callback = function()
@@ -113,29 +114,6 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("lspconfig").pyright.setup({
 	capabilities = capabilities,
-})
-
-require("typescript-tools").setup({
-	on_attach = function(client, bufnr)
-		-- Formatting is handled by eslint via its LSP server
-		client.server_capabilities.documentFormattingProvider = nil
-		client.server_capabilities.documentRangeFormattingProvider = nil
-		on_attach(client, bufnr)
-	end,
-	capabilities = capabilities,
-	settings = {
-		tsserver_file_preferences = {
-			includeInlayParameterNameHints = "all",
-			includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-			includeInlayFunctionParameterTypeHints = true,
-			includeInlayVariableTypeHints = true,
-			includeInlayPropertyDeclarationTypeHints = true,
-			includeInlayFunctionLikeReturnTypeHints = true,
-			includeInlayEnumMemberValueHints = true,
-			-- TODO: revisit
-			importModuleSpecifierPreference = "relative",
-		},
-	},
 })
 
 require("lspconfig").lua_ls.setup({
@@ -174,6 +152,29 @@ require("lspconfig").nil_ls.setup({
 					autoEvalInputs = false,
 				},
 			},
+		},
+	},
+})
+
+require("typescript-tools").setup({
+	on_attach = function(client, bufnr)
+		-- Formatting is handled by eslint via its LSP server
+		client.server_capabilities.documentFormattingProvider = nil
+		client.server_capabilities.documentRangeFormattingProvider = nil
+		on_attach(client, bufnr)
+	end,
+	capabilities = capabilities,
+	settings = {
+		tsserver_file_preferences = {
+			includeInlayParameterNameHints = "all",
+			includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+			includeInlayFunctionParameterTypeHints = true,
+			includeInlayVariableTypeHints = true,
+			includeInlayPropertyDeclarationTypeHints = true,
+			includeInlayFunctionLikeReturnTypeHints = true,
+			includeInlayEnumMemberValueHints = true,
+			-- TODO: revisit
+			importModuleSpecifierPreference = "relative",
 		},
 	},
 })
