a1f1c76600cca96054964967d494efcaeca4ffe8
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2024-11-13 09:12:16 +0200

    chore: update to staging-next

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   # ./result/sw/bin/darwin-rebuild switch --flake ".#eve"
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/staging-next";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
