c788425025b434b9afcf8b70c5904eff15ed4bca
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2024-11-18 11:58:03 +0200

    chore: go back to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   # ./result/sw/bin/darwin-rebuild switch --flake ".#eve"
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/staging-next";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
