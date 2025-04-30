583c77ec53f037605ba92dc00f1145a7ec60ce84
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-04-30 17:14:45 +0300

    eve: disable yabai exclusions

--- machines/eve/configuration.nix
+++ machines/eve/configuration.nix
@@ -31,13 +31,7 @@
       };
       extraConfig = ''
         yabai -m rule --add app='System Settings' manage=off
-        yabai -m rule --add app='krisp' manage=off
         yabai -m config mouse_modifier cmd
-        # Make non-resizable windows floating
-        yabai -m signal --add event=window_created action='yabai -m query --windows --window $YABAI_WINDOW_ID |\
-        ${pkgs.jq}/bin/jq -er ".\"can-resize\" or .\"is-floating\"" ||\
-        yabai -m window $YABAI_WINDOW_ID --toggle float ||\
-        yabai -m window $YABAI_WINDOW_ID --focus'
       '';
     };
     skhd = {
