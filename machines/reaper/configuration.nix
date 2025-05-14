e7e9b3f925399f9752165d5b2b6353a7486f6516
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-05-14 16:42:42 +0300

    chore: remove comment

--- machines/reaper/configuration.nix
+++ machines/reaper/configuration.nix
@@ -119,16 +119,7 @@
 
       # ensure GPU is awake while headless
       nvidiaPersistenced = true;
-
       powerManagement.enable = true;
-
-      # Use the NVidia open source kernel module (not to be confused with the
-      # independent third-party "nouveau" open source driver).
-      # Support is limited to the Turing and later architectures. Full list of
-      # supported GPUs is at:
-      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-      # Only available from driver 515.43.04+
-      # Currently alpha-quality/buggy, so false is currently the recommended setting.
       open = false;
 
       # Enable the Nvidia settings menu,
