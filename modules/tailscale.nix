cfb222bf587be0c3b84720c24060ff067f47b594
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-06-21 20:23:58 +0300

    feat(tailscale): more goodness

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -11,6 +11,16 @@ in
       default = false;
       description = "Enable Tailscale SSH support";
     };
+    magicDns = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Use MagicDNS for resolution";
+    };
+    exitNode = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Advertise this machine as a Tailscale exit node";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -23,12 +33,33 @@ in
 
     services.tailscale = {
       enable = true;
-      extraUpFlags = lib.mkIf cfg.ssh [ "--ssh" ];
+      extraUpFlags = lib.optionals cfg.ssh [ "--ssh" ];
+      extraSetFlags =
+        lib.optionals (!cfg.magicDns) [ "--accept-dns=false" ]
+        ++ lib.optionals cfg.exitNode [ "--advertise-exit-node" ];
       authKeyFile = config.sops.secrets."tailscale-auth-key".path;
     };
 
+    # Enable IP forwarding for exit node functionality
+    boot.kernel.sysctl = lib.mkIf cfg.exitNode (
+      {
+        # TODO: research this, as opposed to "net.ipv4.conf.all.forwarding = 1"
+        "net.ipv4.ip_forward" = true;
+      }
+      // lib.optionalAttrs config.networking.enableIPv6 { "net.ipv6.conf.all.forwarding" = true; }
+    );
+
+    # Configure firewall for exit node traffic
+    networking.firewall = {
+      extraCommands = lib.mkIf cfg.exitNode ''
+        # Allow forwarding to and from Tailscale interface
+        iptables -A FORWARD -i ${config.services.tailscale.interfaceName} -j ACCEPT
+        iptables -A FORWARD -o ${config.services.tailscale.interfaceName} -j ACCEPT
+      '';
+    };
+
     networking.firewall.interfaces.${config.services.tailscale.interfaceName}.allowedTCPPorts =
-      # just the tailscale interface
+      # only affects the tailscale interface
       lib.mkIf cfg.ssh [ 22 ];
   };
 }
