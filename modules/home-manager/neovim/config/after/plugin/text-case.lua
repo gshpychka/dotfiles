e3a6c91bf7740508642075df885984a3ed82e4a6
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-07-19 14:13:00 +0300

    feat(text-case): non-lsp fallback

--- modules/home-manager/neovim/config/after/plugin/text-case.lua
+++ modules/home-manager/neovim/config/after/plugin/text-case.lua
@@ -2,12 +2,30 @@ local textcase = require("textcase")
 textcase.setup({ default_keymappings_enabled = false })
 require("telescope").load_extension("textcase")
 
+-- check if any lsp client supports rename
+local function has_rename_capability()
+  for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
+    if client.supports_method("textDocument/rename") then
+      return true
+    end
+  end
+  return false
+end
+
 vim.keymap.set("n", "<leader>cc", function()
-  textcase.lsp_rename("to_camel_case")
+  if has_rename_capability() then
+    textcase.lsp_rename("to_camel_case")
+  else
+    -- fallback to text conversion only
+    textcase.current_word("to_camel_case")
+  end
 end, { desc = "Rename to camelCase", remap = false })
-vim.keymap.set(
-  "n",
-  "<leader>fcc",
-  "<cmd>TextCaseOpenTelescopeLSPChange<CR>",
-  { desc = "Change to any case with LSP", remap = false }
-)
+
+vim.keymap.set("n", "<leader>fcc", function()
+  if has_rename_capability() then
+    vim.cmd("TextCaseOpenTelescopeLSPChange")
+  else
+    -- fallback to text conversion only
+    vim.cmd("TextCaseOpenTelescope")
+  end
+end, { desc = "Change to any case", remap = false })
