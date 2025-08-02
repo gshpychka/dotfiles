f4af99e808e76680aacb4243b65ca0a6c7b07f4d
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-08-02 22:26:48 +0300

    feat(nvim): rename

--- modules/home-manager/neovim/config/after/plugin/snacks.lua
+++ modules/home-manager/neovim/config/after/plugin/snacks.lua
@@ -32,17 +32,3 @@ vim.keymap.set("n", "<leader>cR", function()
   require("snacks").rename.rename_file()
 end, { desc = "Rename File" })
 
--- nvim-tree integration for rename events
-local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
-vim.api.nvim_create_autocmd("User", {
-  pattern = "NvimTreeSetup",
-  callback = function()
-    local events = require("nvim-tree.api").events
-    events.subscribe(events.Event.NodeRenamed, function(data)
-      if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
-        prev = data
-        require("snacks").rename.on_rename_file(data.old_name, data.new_name)
-      end
-    end)
-  end,
-})
