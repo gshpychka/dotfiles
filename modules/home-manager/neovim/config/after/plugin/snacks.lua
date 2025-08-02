ed08fe7ca77481a97efa94e5dfe3723f3d64090d
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-08-02 21:34:10 +0300

    feat(nvim): snacks rename file

--- modules/home-manager/neovim/config/after/plugin/snacks.lua
+++ modules/home-manager/neovim/config/after/plugin/snacks.lua
@@ -14,9 +14,10 @@ snacks.setup({
       hl = "CursorLineNr",
     },
   },
-  input = { enabled = false },     -- using noice.nvim for input dialogs
-  notifier = { enabled = false },  -- using noice.nvim for notifications
+  input = { enabled = false },    -- using noice.nvim for input dialogs
+  notifier = { enabled = false }, -- using noice.nvim for notifications
   quickfile = { enabled = false },
+  rename = { enabled = true },
   scroll = { enabled = false },
   statuscolumn = { enabled = false }, -- gitsigns
   words = { enabled = false },
@@ -25,3 +26,23 @@ snacks.setup({
   },
 })
 
+-- https://github.com/folke/snacks.nvim/blob/main/docs/rename.md
+-- Keybinding for file renaming
+vim.keymap.set("n", "<leader>cR", function()
+  require("snacks").rename.rename_file()
+end, { desc = "Rename File" })
+
+-- nvim-tree integration for rename events
+local prev = { new_name = "", old_name = "" } -- Prevents duplicate events
+vim.api.nvim_create_autocmd("User", {
+  pattern = "NvimTreeSetup",
+  callback = function()
+    local events = require("nvim-tree.api").events
+    events.subscribe(events.Event.NodeRenamed, function(data)
+      if prev.new_name ~= data.new_name or prev.old_name ~= data.old_name then
+        prev = data
+        require("snacks").rename.on_rename_file(data.old_name, data.new_name)
+      end
+    end)
+  end,
+})
