75cdb9ab01d88370e9141be0841b3c71be241c3b
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-08-09 18:30:32 +0300

    chore: update

--- overlays/claudecode-nvim.nix
+++ overlays/claudecode-nvim.nix
@@ -1,12 +1,12 @@
 final: prev: {
   vimPlugins = prev.vimPlugins // {
     claudecode-nvim = prev.vimPlugins.claudecode-nvim.overrideAttrs (oldAttrs: {
-      version = "2025-07-30";
+      version = "2025-08-08";
       src = final.fetchFromGitHub {
         owner = "coder";
         repo = "claudecode.nvim";
-        rev = "477009003cbec7e6088dbbeab46aba80f461d5f0";
-        sha256 = "P2FELIY8roeII4kVgk5BEHWkhelJCsaV6PyMIkEpC8I=";
+        rev = "985b4b117ea13ec85c92830ecac8f63543dd5ead";
+        sha256 = "";
       };
     });
   };
