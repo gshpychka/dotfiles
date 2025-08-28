08cf05691c8f9fe893b1441a2b75d2f1dc3f0862
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-08-28 10:31:43 +0300

    feat: wip: ssh config

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -17,21 +17,34 @@ in
     programs = {
       ssh = {
         enable = true;
+        enableDefaultConfig = false;
         extraConfig = ''
           CanonicalizeHostname yes
-          CanonicalDomains ${domain}
-
           # only canonicalize hosts with no dots
           CanonicalizeMaxDots 0
+          CanonicalDomains ${domain}
         '';
 
-        # this is needed because otherwise (if we leave the default value of `false`),
-        # our override will not be applied if domain canonicalization happens -
-        # since the value will be set on the first pass and first one wins
-        # we will set it to false on the second pass
-        forwardAgent = true;
-
         matchBlocks = {
+          "*" = {
+            # this is needed because otherwise (if we leave the default value of `false`),
+            # our override will not be applied if domain canonicalization happens -
+            # since the value will be set on the first pass and first one wins
+            # we will set it to false on the second pass
+            # TODO: investigate
+            # forwardAgent = true;
+
+            # preserve default config after https://github.com/nix-community/home-manager/pull/7655
+            compression = false;
+            addKeysToAgent = "no";
+            hashKnownHosts = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts";
+            controlPath = "${config.home.homeDirectory}/.ssh/master-%r@%h:%p";
+            controlMaster = "no";
+            controlPersist = "no";
+          };
 
           # orders matters for SSH, since the first value wins (no overrides later)
           # The order of the attrs specified here has no effect on the resulting file
@@ -42,10 +55,10 @@ in
             user = "pi";
           };
 
+          # these two are for future use - no custom config for now
           reaper = lib.hm.dag.entryBefore [ "local" ] {
             match = "final host reaper.${domain}";
           };
-
           hoard = lib.hm.dag.entryBefore [ "local" ] {
             match = "final host hoard.${domain}";
           };
