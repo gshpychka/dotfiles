ca41a517376819d9e0db349f196401cfad1482c6
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-08-29 18:12:04 +0300

    chore: remove comment

--- modules/system/common/nix-config.nix
+++ modules/system/common/nix-config.nix
@@ -7,7 +7,6 @@
         allowed-users = [ config.my.user ];
         trusted-users = [ config.my.user ];
 
-        # Binary caches - consolidated configuration
         extra-substituters = [
           "https://nix-community.cachix.org"
         ];
