cfd3a5645599bf4466d8e032aef40964ff130f2b
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-12-08 15:18:39 +0200

    feat(ccusage): remove custom package

--- packages/default.nix
+++ packages/default.nix
@@ -3,9 +3,7 @@
   ...
 }:
 {
-  ccusage = pkgs.callPackage ./ccusage.nix { };
   vimPlugins = pkgs.vimPlugins // {
     ts-error-translator-nvim = pkgs.callPackage ./ts-error-translator-nvim.nix { };
   };
 }
-
