686152a05da950ab5bacf96c1863362c5f6fcb39
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-12-09 23:28:57 +0200

    chore(buoy): don't use cloudflare cert

--- machines/buoy/cloudflare-tunnel.nix
+++ machines/buoy/cloudflare-tunnel.nix
@@ -5,26 +5,22 @@
 {
   services.cloudflared = {
     enable = true;
-    certificateFile = config.sops.secrets.cloudflare-cert.path;
     # cloudflared tunnel create buoy-tunnel
     tunnels.buoy-tunnel = {
       credentialsFile = config.sops.secrets.cloudflare-tunnel.path;
       default = "http_status:404";
       ingress = {
         # cloudflared tunnel route dns buoy-tunnel <hostname>
-        "status.${config.my.domain}" = "http://localhost:${toString config.services.gatus.settings.web.port}";
+        "status.${config.my.domain}" =
+          "http://localhost:${toString config.services.gatus.settings.web.port}";
       };
     };
   };
 
   sops.secrets = {
-    cloudflare-cert = {
-      sopsFile = ../../secrets/common/cloudflare-cert.pem;
-      mode = "0440";
-      format = "binary";
-    };
     cloudflare-tunnel = {
       sopsFile = ../../secrets/buoy/cloudflare-tunnel.json;
+      restartUnits = [ "cloudflared-tunnel-buoy-tunnel.service" ];
       mode = "0440";
       format = "json";
       key = ""; # we want the entire file
