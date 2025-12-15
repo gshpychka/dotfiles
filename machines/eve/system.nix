b7200b0449ece65b93f1997238670de4bde985cd
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-12-15 18:36:25 +0200

    fix(eve): attempt to fix ulimit

--- machines/eve/system.nix
+++ machines/eve/system.nix
@@ -58,4 +58,19 @@
 
   };
   my.sudoTouchId.enable = true;
+
+  # set ulimit for open files to 4096
+  # otherwise, `nix flake update` fails with "Too many open files"
+  # https://github.com/NixOS/nix/issues/6557
+  # https://github.com/NixOS/nix/pull/5726
+  launchd.user.agents.ulimit = {
+    serviceConfig = {
+      ProgramArguments = [
+        "${pkgs.bash}/bin/bash"
+        "-c"
+        "launchctl limit maxfiles 4096 unlimited"
+      ];
+      RunAtLoad = true;
+    };
+  };
 }
