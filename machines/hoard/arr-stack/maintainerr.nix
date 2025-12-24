d71608838276417b4ad2a4503652ea3e78e72624
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2025-12-24 09:50:02 +0200

    chore(maintainerr): update

--- machines/hoard/arr-stack/maintainerr.nix
+++ machines/hoard/arr-stack/maintainerr.nix
@@ -27,7 +27,7 @@ in
     };
   };
   virtualisation.oci-containers.containers.maintainerr = {
-    image = "ghcr.io/maintainerr/maintainerr:2.22";
+    image = "ghcr.io/maintainerr/maintainerr:2.25";
     volumes = [
       "/var/lib/maintainerr:/opt/data"
     ];
