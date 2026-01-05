b282b0291048c4a1a0afd7a66df26a509aee0828
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2026-01-05 20:21:04 +0200

    chore(llm-agents.nix): update binary cache

--- modules/system/common/nix-config.nix
+++ modules/system/common/nix-config.nix
@@ -16,11 +16,11 @@
 
           extra-substituters = [
             "https://nix-community.cachix.org"
-            "https://numtide.cachix.org"
+            "https://cache.numtide.com"
           ];
           extra-trusted-public-keys = [
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+            "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
           ];
           experimental-features = [
             "nix-command"
