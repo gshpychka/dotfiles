a8511da651e6a48e76c458d6dfa0a3002b74c410
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2026-05-20 15:24:45 +0300

    chore(ssh): remove matchBlocks

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -26,43 +26,45 @@ in
         '';
         enableDefaultConfig = false;
 
-        matchBlocks = {
+        settings = {
 
           # order matters for SSH, since the first value wins (no overrides later)
           # The order of the attrs specified here has no effect on the resulting file
           # because HM orders these as a DAG, so we need to specify the dependencies ourselves
+          # Short attr names are used (rather than literal `Match ...` keys) so the DAG
+          # references stay stable and readable.
 
           iso = lib.hm.dag.entryBefore [ "local" ] {
-            match = "final host iso.${domain}";
-            user = "nixos";
+            header = "Match final host iso.${domain}";
+            User = "nixos";
           };
 
           harbor = lib.hm.dag.entryBefore [ "local" ] {
-            match = "final host harbor.${domain}";
-            user = "pi";
+            header = "Match final host harbor.${domain}";
+            User = "pi";
           };
 
           reaper = lib.hm.dag.entryBefore [ "local" ] {
-            match = "final host reaper.${domain}";
+            header = "Match final host reaper.${domain}";
           };
 
           hoard = lib.hm.dag.entryBefore [ "local" ] {
-            match = "final host hoard.${domain}";
+            header = "Match final host hoard.${domain}";
           };
 
           kodi = lib.hm.dag.entryBefore [ "local" ] {
-            match = "final host kodi.${domain}";
-            user = "root";
-            forwardAgent = false;
-            setEnv = {
+            header = "Match final host kodi.${domain}";
+            User = "root";
+            ForwardAgent = false;
+            SetEnv = {
               TERM = "xterm-256color";
             };
           };
 
           local = {
-            match = "final host *.${domain}";
-            user = osConfig.my.user;
-            forwardAgent = true;
+            header = "Match final host *.${domain}";
+            User = osConfig.my.user;
+            ForwardAgent = true;
           };
 
           remote = {
@@ -71,23 +73,23 @@ in
             # (i.e. before canonicalization)
             # N.B. we need to use `final` as opposed to `canonical`,
             # since the latter will not match if no canonicalization happens
-            match = "final host !*.${domain}";
-            forwardAgent = false;
-            setEnv = {
+            header = "Match final host !*.${domain}";
+            ForwardAgent = false;
+            SetEnv = {
               # avoid compatibility issues with Ghostty
               TERM = "xterm-256color";
             };
           };
           "*" = {
-            addKeysToAgent = "no";
-            compression = false;
-            serverAliveInterval = 0;
-            serverAliveCountMax = 3;
-            hashKnownHosts = false;
-            userKnownHostsFile = "~/.ssh/known_hosts";
-            controlMaster = "no";
-            controlPath = "~/.ssh/master-%r@%n:%p";
-            controlPersist = "no";
+            AddKeysToAgent = "no";
+            Compression = false;
+            ServerAliveInterval = 0;
+            ServerAliveCountMax = 3;
+            HashKnownHosts = false;
+            UserKnownHostsFile = "~/.ssh/known_hosts";
+            ControlMaster = "no";
+            ControlPath = "~/.ssh/master-%r@%n:%p";
+            ControlPersist = "no";
           };
         };
       };
